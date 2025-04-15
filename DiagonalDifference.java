dc82c32e27ee69d55a033774ca22c368546f2a20
howaydaAbbas <97281820+howaydaAbbas@users.noreply.github.com>
2025-04-15 13:23:46 +0200

    solve Diagonal Difference

---
+++ DiagonalDifference.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class DiagonalDifference {
+
+    public static int getDiagonalDifference(List<List<Integer>> arr) {
+
+        int firstDiagonalSum = 0;
+        int secondDiagonalSum = 0;
+        int diagonalSize = arr.size();
+        for (int i = 0; i < diagonalSize; i++){
+            List<Integer> currentDiagonal = arr.get(i);
+            firstDiagonalSum += currentDiagonal.get(i);
+            secondDiagonalSum += currentDiagonal.get(diagonalSize-i-1);
+        }
+        return Math.abs(firstDiagonalSum-secondDiagonalSum);
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        // Diagonal Difference
+        List<List<Integer>> arr = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        first.add(2);
+        first.add(3);
+        first.add(4);
+        List<Integer> second = new ArrayList<>();
+        second.add(5);
+        second.add(6);
+        second.add(7);
+        second.add(8);
+        List<Integer> third = new ArrayList<>();
+        third.add(9);
+        third.add(10);
+        third.add(11);
+        third.add(12);
+        List<Integer> fourth = new ArrayList<>();
+        fourth.add(13);
+        fourth.add(14);
+        fourth.add(15);
+        fourth.add(20);
+        arr.add(first);
+        arr.add(second);
+        arr.add(third);
+        arr.add(fourth);
+        System.out.println(DiagonalDifference.getDiagonalDifference(arr));
+    }
+}
